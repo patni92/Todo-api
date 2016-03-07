c4008761a5708a7a01de956467e9636276522b9a
Patrik Nilsson <pn222en@student.lnu.se>
2016-03-07 14:24:25 +0100

    Add DELETE todos route

--- server.js
+++ server.js
@@ -45,6 +45,24 @@ app.post("/todos", function (req, res) {
     res.json(body);
 });
 
+app.delete("/todos/:id", function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {
+        id: todoId,
+    });
+
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).json({
+            "error": "no todo found with that id",
+        });
+    }
+
+    todos = _.without(todos, matchedTodo);
+
+});
+
 app.listen(PORT, function () {
     console.log("express listening on port " + PORT);
 });
