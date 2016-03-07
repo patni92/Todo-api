03c1b153e111ed4a5a6ac0a89acbe67ab412c24a
Patrik Nilsson <pn222en@student.lnu.se>
2016-03-07 12:12:17 +0100

    add post for todos

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: "Meet mom for lunch",
-    completed: false,
-}, {
-    id: 2,
-    description: "go to market",
-    completed: false,
-}, {
-    id: 3,
-    description: "Go and study",
-    completed: true,
-},
-];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get("/todos", function (req, res) {
     res.json(todos);
@@ -38,6 +29,14 @@ app.get("/todos/:id", function (req, res) {
     } else {
         res.status(404).send();
     }
+});
+
+app.post("/todos", function (req, res) {
+    var body = req.body;
+    body.id = todoNextId;
+    todoNextId += 1;
+    todos.push(body);
+    res.json(body);
 });
 
 app.listen(PORT, function () {
