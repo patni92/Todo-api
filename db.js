809a41ffd22fabb21b456f635d80c35045ff6d53
Patrik Nilsson <pn222en@student.lnu.se>
2016-03-11 16:05:56 +0100

    Update PUT /todos/:id to use sequelize

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
 var Sequelize = require('sequelize');
-var env = process.env.NODE_ENV;
+var env = process.env.NODE_ENV || 'development';
 var sequelize;
-if (env === "production") {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: "postgres"
-    });
+
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
 } else {
-    sequelize = new Sequelize(undefined, undefined, undefined, {
-    	'dialect': 'sqlite',
-    	'storage': __dirname + '/data/dev-todo-api.sqlite'
-    });
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
 }
+
 var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
