d6cfb1bab5007fa75989032a6da16691db5d5c0a
Patrik Nilsson <pn222en@student.lnu.se>
2016-03-14 15:30:54 +0100

    Add login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -1,43 +1,73 @@
 var passwordHash = require('password-hash');
 var _ = require('underscore');
+
+
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define("user", {
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-            }
-        },
-        password_hash: {
-            type: DataTypes.STRING,
-        },
-        password: {
-            type: DataTypes.VIRTUAL,
-            allowNull: false,
-            validate: {
-                len: [7,100]
-            },
-            set: function (value) {
-                var hahsedPassword = passwordHash.generate(value);
-                this.setDataValue("password", value);
-                this.setDataValue("password_hash", hahsedPassword);
-            }
-        }
-    }, {
-        hooks: {
-            beforeValidate: function(user, options) {
-                if(typeof user.email === "string") {
-                    user.email = user.email.toLowerCase();
-                }
-            }
-        },
-        instanceMethods:  {
-            toPublicJSON: function () {
-                var json = this.toJSON();
-                return _.pick(json, "id", "email", "createdAt", "updatedAt")
-            }
-        }
-    });
+	var user = sequelize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		
+		password_hash: {
+			type: DataTypes.STRING
+		},
+		password: {
+			type: DataTypes.VIRTUAL,
+			allowNull: false,
+			validate: {
+				len: [7, 100]
+			},
+			set: function(value) {
+				var hashedPassword =  passwordHash.generate(value);
+
+				this.setDataValue('password', value);
+				this.setDataValue('password_hash', hashedPassword);
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				// user.email
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !passwordHash.verify(body.password, user.get("password_hash"))) {
+							return reject();
+						}
+
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+				});
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			}
+		}
+	});
+
+	return user;
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var passwordHash = require('password-hash');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -127,8 +128,21 @@ app.post("/users", function(req, res) {
     });
 });
 
+app.post("/users/login", function(req, res) {
+    var body = _.pick(req.body, "email", "password");
 
-db.sequelize.sync().then(function() {
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON())
+    }, function () {
+        res.status(401).send();
+    });
+
+
+
+});
+
+
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
